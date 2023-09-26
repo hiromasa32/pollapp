0699e32bb5faa5fc0c588f42243f34be3d8ba227
hiromasa32 <146090712+hiromasa32@users.noreply.github.com>
2023-09-26 21:05:49 +0900

    Add files via upload

---
+++ php/db/datasource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace db;
+
+use PDO;
+
+// 必ず同じDBへのコネクション（同じインスタンス）を取得する
+#[\AllowDynamicProperties]
+class PDOSingleton {
+    private static $singleton;      // DBへのコネクションが保持される
+
+    // 外部からは呼び出せない
+    private function __construct($dsn, $username, $password) {
+
+        $this->conn = new PDO($dsn, $username, $password);
+        $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
+
+    // 1回目はコンストラクタを実行する
+    public static function getInstance($dsn, $username, $password) {
+        if (!isset(self::$singleton)) {
+            $instance = new PDOSingleton($dsn, $username, $password);
+            self::$singleton = $instance->conn;
+        }
+        return self::$singleton;
+    }
+}
+
+class DataSource {
+
+    private $conn;
+    private $sqlResult;
+    public const CLS = 'cls';
+
+    public function __construct($host = 'mysql1.php.xdomain.ne.jp', $port = '3306', $dbName = 'kunren0622_pollapp', $username = 'kunren0622_test', $password = 'Eg6bnGZd') {
+
+        $dsn = "mysql:host={$host};port={$port};dbname={$dbName};";
+        $this->conn = PDOSingleton::getInstance($dsn, $username, $password);
+    }
+
+    public function select($sql = "", $params = [], $type = '', $cls = '') {
+
+        $stmt = $this->executeSql($sql, $params);
+
+        if ($type === static::CLS) {
+
+            return $stmt->fetchAll(PDO::FETCH_CLASS, $cls);
+        } else {
+
+            return $stmt->fetchAll();
+        }
+    }
+
+    public function execute($sql = "", $params = []) {
+
+        $this->executeSql($sql, $params);
+        return  $this->sqlResult;
+    }
+
+    public function selectOne($sql = "", $params = [], $type = '', $cls = '') {
+
+        $result = $this->select($sql, $params, $type, $cls);
+        return count($result) > 0 ? $result[0] : false;
+    }
+
+    public function begin() {
+
+        $this->conn->beginTransaction();
+    }
+
+    public function commit() {
+
+        $this->conn->commit();
+    }
+
+    public function rollback() {
+
+        $this->conn->rollback();
+    }
+
+    private function executeSql($sql, $params) {
+
+        $stmt = $this->conn->prepare($sql);
+        $this->sqlResult = $stmt->execute($params);
+        return $stmt;
+    }
+}
